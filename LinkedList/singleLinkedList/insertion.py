bc1aebaaa09f79a0fe6351caa160835cbf1a4113
sankar58 <ayudhamsankar@gmail.com>
2026-01-08 23:02:09 +0530

    linked list insertion

---
+++ LinkedList/singleLinkedList/insertion.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,value=None):
+        self.value =value
+        self.next=None
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail=None
+    def __iter__(self):
+        temp=self.head
+        while temp:
+            yield temp
+            temp=temp.next
+
+    def insertion(self,value,location):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        elif location==0:
+            new_node.next=self.head
+            self.head=new_node
+        elif location ==1:
+            self.tail.next=new_node
+            self.tail=new_node
+            new_node.next=None
+        else:
+            temp=self.head
+            index=0
+            while index<location-1:
+                temp=temp.next
+                index+=1
+            nextnode=temp.next
+            temp.next=new_node
+            new_node.next=nextnode
+
+
+my_linked_list=SingleLinkedList()
+my_linked_list.insertion(1,0)
+my_linked_list.insertion(2,1)
+my_linked_list.insertion(2,1)
+my_linked_list.insertion(2,1)
+my_linked_list.insertion(5,2)
+print([temp.value for temp in my_linked_list])
