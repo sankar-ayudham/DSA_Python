2cd7dfa753ab81edef8fa789b100bad899a7ac16
sankar58 <ayudhamsankar@gmail.com>
2026-01-08 23:03:51 +0530

    linked list traversal

---
+++ LinkedList/singleLinkedList/travesal.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self,value=None):
+        self.value = value
+        self.next = None
+
+
+class SingleLinkedlist:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def __iter__(self):
+        temp= self.head
+        while temp:
+            yield temp
+            temp=temp.next
+
+    def insertion(self, value, location):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif location == 0:
+            new_node.next = self.head
+            self.head = new_node
+        elif location == 1:
+            self.tail.next = new_node
+            self.tail = new_node
+            new_node.next = None
+        else:
+            temp = self.head
+            index = 0
+            while index < location - 1:
+                temp = temp.next
+                index += 1
+            nextnode = temp.next
+            temp.next = new_node
+            new_node.next = nextnode
+    def traversal(self):
+        if self.head is None:
+            print("the linked list is empty")
+        else:
+            temp=self.head
+            while temp is not None:
+                print(temp.value)
+                temp=temp.next
+
+my_linked_list = SingleLinkedlist()
+my_linked_list.insertion(1, 0)
+my_linked_list.insertion(2, 1)
+my_linked_list.insertion(2, 1)
+my_linked_list.insertion(2, 1)
+my_linked_list.insertion(5, 2)
+my_linked_list.traversal()
+print([temp.value for temp in my_linked_list])
