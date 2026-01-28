5577902b5c4f8c31f45ff82021f82b932c80a772
sankar58 <ayudhamsankar@gmail.com>
2026-01-28 21:49:39 +0530

    deleting node in binary tree

---
+++ binary_Tree/using_list/deleteBt.py
@@ -0,0 +1,22 @@
+class BinaryTree:
+    def __init__(self,size):
+        self.list=size*[None]
+        self.lastUsedindex=0
+        self.maxsize=size
+
+    def insert(self,value):
+        if self.lastUsedindex+1==self.maxsize:
+            return "the binary treee is full"
+        self.list[self.lastUsedindex+1]=value
+        self.lastUsedindex +=1
+        return "inserted successfully"
+    def delete(self):
+        self.list=None
+        return "binary tree successfully deleted"
+
+BTree= BinaryTree(5)
+print(BTree.insert("drinks"))
+print(BTree.insert("hot"))
+print(BTree.insert("cold"))
+print(BTree.delete())
+
