5f128067b57c19f8d2074545f840a575c7e1922f
sankar58 <ayudhamsankar@gmail.com>
2026-02-05 21:49:34 +0530

    creating tuple

---
+++ Tuple/creating.py
@@ -0,0 +1,14 @@
+my_tuple=("a","b","c","d")
+print(my_tuple)
+
+# method 2
+my_tuple1="a","b","c","d"
+print(my_tuple1)
+
+# method3
+my_tuple2=tuple("abcd")
+print(my_tuple)
+
+# if want to include one element in the tuple u must use "," else it treated as string
+my_tuple3=("a",)
+print(my_tuple3)
